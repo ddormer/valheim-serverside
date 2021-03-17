b95639c85b8e61d12c18ea950bfc2bba0023f099
Darren Dormer <me@darren.io>
2021-03-17 14:43:41 +0100

    Re-implement ZNetScene.CreateDestroyObjects

--- ServersidePlugin.cs
+++ ServersidePlugin.cs
@@ -21,5 +21,28 @@ namespace Valheim_Serverside
 			UnityEngine.Debug.Log("Valheim Serverside installed");
 			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
 		}
+
+		[HarmonyPatch(typeof(ZNetScene), "CreateDestroyObjects")]
+		private class CreateDestroyObjects_Patch
+		{
+			private static bool Prefix(ZNetScene __instance)
+			{
+				if (!ZNet.instance || !ZNet.instance.IsServer())
+				{
+					return true;
+				}
+				List<ZDO> m_tempCurrentObjects = new List<ZDO>();
+				List<ZDO> m_tempCurrentDistantObjects = new List<ZDO>();
+				foreach (ZNetPeer znetPeer in ZNet.instance.GetConnectedPeers())
+				{
+					Vector2i zone = ZoneSystem.instance.GetZone(znetPeer.GetRefPos());
+					ZDOMan.instance.FindSectorObjects(zone, ZoneSystem.instance.m_activeArea, ZoneSystem.instance.m_activeDistantArea, m_tempCurrentObjects, m_tempCurrentDistantObjects);
+				}
+
+				Traverse.Create(__instance).Method("CreateObjects", m_tempCurrentObjects, m_tempCurrentDistantObjects).GetValue();
+				Traverse.Create(__instance).Method("RemoveObjects", m_tempCurrentObjects, m_tempCurrentDistantObjects).GetValue();
+				return false;
+			}
+		}
 	}
 }
