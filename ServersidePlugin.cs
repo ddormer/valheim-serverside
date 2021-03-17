c880c4e86992adf803c4eb29cefd0c2f0dae70ec
Darren Dormer <me@darren.io>
2021-03-17 14:41:20 +0100

    Initial commit

---
+++ ServersidePlugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using System;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace Valheim_Serverside
+{
+	[BepInPlugin("MVP.Valheim_Serverside", "Valheim Serverside", "1.0.0.0")]
+	public class ServersidePlugin : BaseUnityPlugin
+	{
+		void Awake()
+		{
+			UnityEngine.Debug.Log("Valheim Serverside installed");
+			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+		}
+	}
+}
