b08b88cea98c0223a5e2602cd98ab21f53474496
Darren Dormer <me@darren.io>
2021-04-15 22:17:06 +0200

    Update update-bbcode-readme.sh

--- bin/update-bbcode-readme.sh
+++ bin/update-bbcode-readme.sh
@@ -9,6 +9,8 @@ git pull github ${GITHUB_REF} --ff-only
 npm install markdown-to-bbcode
 node --experimental-modules bin/build-bbcode.mjs
 
+git diff-files README.bbcode
+
 if ! `git diff-files --quiet README.bbcode` ; then
   echo "No README.bbcode changes found, exiting."
   exit 0
