3a1bdac6e4c825793094117adcc0bbaca17dde3b
unknown <jayess@jayess.za.net>
2021-04-18 15:28:29 +0200

    Revert "Revert Piece ownership change."
    This reverts commit 8ae4fef8dc832e1fa864742e28bbabcccebba147.

--- src/Valheim_Serverside/ServersidePlugin.cs
+++ src/Valheim_Serverside/ServersidePlugin.cs
@@ -418,6 +418,55 @@ namespace Valheim_Serverside
 			}
 		}
 
+		[HarmonyPatch(typeof(ZDOMan), "RPC_ZDOData")]
+		public class ZDOMan_RPC_ZDODataPatch
+		{
+			static void CheckShouldOwn(ZDO zdo, bool isNew)
+			{
+				long myid = ZNet.instance.GetUID();
+				long owner = zdo.m_owner;
+				if (isNew && owner != 0L && owner != myid)
+				{
+					int prefabHash = zdo.GetPrefab();
+					GameObject prefab = ZNetScene.instance.GetPrefab(prefabHash);
+					// Only take control of building pieces for now
+					// TODO: See if this should be expanded
+					if (prefab != null && prefab.GetComponent<Piece>() != null)
+					{
+						#if DEBUG
+						context.Logger.LogInfo($"Taking ownership of new ZDO (player id: {owner} id: {zdo.m_uid} name: {prefab.name}");
+						#endif
+						zdo.SetOwner(myid);
+						return;
+					}
+				}
+			}
+
+			public static void ILManipulator(ILContext il)
+			{
+				int idx_stZDO = 0;
+				int idx_stZDOIsNew = 0;
+
+				new ILCursor(il)
+					.GotoNext(
+						i => i.MatchCall<ZDOMan>("CreateNewZDO"),
+						i => i.MatchStloc(out idx_stZDO),
+						i => i.MatchLdcI4(out _),
+						i => i.MatchStloc(out idx_stZDOIsNew)
+					)
+					.GotoNext(MoveType.After,
+						i => i.MatchLdloc(out _),
+						i => i.MatchLdloc(out _),
+						i => i.MatchCallvirt<ZDO>("Deserialize")
+					)
+					.Emit(OC.Ldloc, idx_stZDO)
+					.Emit(OC.Ldloc, idx_stZDOIsNew)
+					.Emit(OC.Call, AccessTools.Method(typeof(ZDOMan_RPC_ZDODataPatch),
+													  nameof(ZDOMan_RPC_ZDODataPatch.CheckShouldOwn)))
+				;
+			}
+		}
+
 		[HarmonyPatch(typeof(ZRoutedRpc), "RouteRPC")]
 		static class ZRoutedRpc_RouteRPC_Patch
 		/*
