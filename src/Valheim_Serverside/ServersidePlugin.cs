3ee2cb92a11f3311c939b94b5e384e731902739e
Darren Dormer <me@darren.io>
2021-09-20 21:20:00 +0200

    Bump version to 1.1.3

--- src/Valheim_Serverside/ServersidePlugin.cs
+++ src/Valheim_Serverside/ServersidePlugin.cs
@@ -10,9 +10,9 @@ namespace Valheim_Serverside
 {
 
 	[Harmony]
-	[BepInPlugin("MVP.Valheim_Serverside_Simulations", "Serverside Simulations", "1.1.2")]
-  [BepInDependency(ValheimPlusPluginId, BepInDependency.DependencyFlags.SoftDependency)]
-  
+	[BepInPlugin("MVP.Valheim_Serverside_Simulations", "Serverside Simulations", "1.1.3")]
+	[BepInDependency(ValheimPlusPluginId, BepInDependency.DependencyFlags.SoftDependency)]
+
 	public class ServersidePlugin : BaseUnityPlugin
 	{
 
