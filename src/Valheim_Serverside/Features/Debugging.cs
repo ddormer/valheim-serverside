32f78f885bd68c14e4640098d8782e84c4beb157
William Seligmann <jayess@jayess.za.net>
2023-11-13 13:13:45 +0200

    Improve performance of finding objects in CreateDestroyObjects_Patch.

--- src/Valheim_Serverside/Features/Debugging.cs
+++ src/Valheim_Serverside/Features/Debugging.cs
@@ -2,6 +2,10 @@
 using PluginConfiguration;
 using Requirements;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
 
 namespace Valheim_Serverside.Features
 {
@@ -17,6 +21,11 @@ namespace Valheim_Serverside.Features
 		[HarmonyPatch(typeof(Chat), "RPC_ChatMessage")]
 		public static class Chat_RPC_ChatMessage_Patch
 		{
+			private static HashSet<Vector2i> m_tempNearSectors = new HashSet<Vector2i>();
+			private static HashSet<Vector2i> m_tempDistantSectors = new HashSet<Vector2i>();
+			private static List<ZDO> m_tempCurrentObjects = new List<ZDO>();
+			private static List<ZDO> m_tempCurrentDistantObjects = new List<ZDO>();
+
 			static void Prefix(ref long sender, ref string text)
 			{
 				ZNetPeer peer = ZNet.instance.GetPeer(sender);
@@ -36,8 +45,43 @@ namespace Valheim_Serverside.Features
 				{
 					Configuration.maxObjectsPerFrame.Value = Convert.ToInt32(text.Split(' ').GetValue(1));
 				}
+				else if (text == "testfindobjects")
+				{
+					var sw = new Stopwatch();
+					sw.Start();
+					for (int i = 0; i < 1000; i++)
+					{
+						m_tempCurrentObjects.Clear();
+						m_tempCurrentDistantObjects.Clear();
+						foreach (ZNetPeer znetPeer in ZNet.instance.GetConnectedPeers())
+						{
+							Vector2i zone = ZoneSystem.instance.GetZone(znetPeer.GetRefPos());
+							ZDOMan.instance.FindSectorObjects(zone, ZoneSystem.instance.m_activeArea, ZoneSystem.instance.m_activeDistantArea, m_tempCurrentObjects, m_tempCurrentDistantObjects);
+						}
+
+						m_tempCurrentDistantObjects = m_tempCurrentDistantObjects.Distinct().ToList();
+						m_tempCurrentObjects = m_tempCurrentObjects.Distinct().ToList();
+					}
+					ServersidePlugin.logger.LogInfo($"Elapsed: {sw.ElapsedMilliseconds}ms");
+				}
+				else if (text == "testfindobjects2")
+				{
+					var sw = new Stopwatch();
+					sw.Start();
+					for (int i = 0; i < 1000; i++)
+					{
+						m_tempNearSectors.Clear();
+						m_tempDistantSectors.Clear();
+						m_tempCurrentObjects.Clear();
+						m_tempCurrentObjects.Clear();
+
+						Utilities.FindAllActiveSectors(ZoneSystem.instance.m_activeArea, ZoneSystem.instance.m_activeDistantArea, m_tempNearSectors, m_tempDistantSectors);
+						Utilities.FindObjectsInSectors(m_tempNearSectors, m_tempCurrentObjects);
+						Utilities.FindDistantObjectsInSectors(m_tempDistantSectors, m_tempCurrentObjects);
+					}
+					ServersidePlugin.logger.LogInfo($"Elapsed: {sw.ElapsedMilliseconds}ms");
+				}
 			}
 		}
 	}
-
 }
--- src/Valheim_Serverside/Utils.cs
+++ src/Valheim_Serverside/Utils.cs
@@ -1,4 +1,8 @@
-﻿namespace Valheim_Serverside
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System;
+
+namespace Valheim_Serverside
 {
 	public class Utilities
 	{
@@ -8,6 +12,73 @@
 			return true;
 #endif
 			return false;
+		}
+
+		public static void FindActiveSectors(Vector2i sector, int area, int distantArea, HashSet<Vector2i> nearbySectors, HashSet<Vector2i> distantSectors = null)
+		/*
+			Same logic as as ZDOMan.FindSectorObjects; output sectors instead of objects.
+		*/
+		{
+			nearbySectors.Add(sector);
+			for (int i = 1; i <= area; i++)
+			{
+				for (int j = sector.x - i; j <= sector.x + i; j++)
+				{
+					nearbySectors.Add(new Vector2i(j, sector.y - i));
+					nearbySectors.Add(new Vector2i(j, sector.y + i));
+				}
+				for (int k = sector.y - i + 1; k <= sector.y + i - 1; k++)
+				{
+					nearbySectors.Add(new Vector2i(sector.x - i, k));
+					nearbySectors.Add(new Vector2i(sector.x + i, k));
+				}
+			}
+
+			HashSet<Vector2i> sectors = (distantSectors != null) ? distantSectors : nearbySectors;
+			for (int l = area + 1; l <= area + distantArea; l++)
+			{
+				for (int m = sector.x - l; m <= sector.x + l; m++)
+				{
+					sectors.Add(new Vector2i(m, sector.y - l));
+					sectors.Add(new Vector2i(m, sector.y + l));
+				}
+				for (int n = sector.y - l + 1; n <= sector.y + l - 1; n++)
+				{
+					sectors.Add(new Vector2i(sector.x - l, n));
+					sectors.Add(new Vector2i(sector.x + l, n));
+				}
+			}
+		}
+
+		public static void FindAllActiveSectors(int area, int distantArea, HashSet<Vector2i> nearbySectors, HashSet<Vector2i> distantSectors = null)
+		{
+			foreach (ZNetPeer peer in ZNet.instance.GetPeers())
+			{
+				Vector2i sector = ZoneSystem.instance.GetZone(peer.GetRefPos());
+				FindActiveSectors(sector, area, distantArea, nearbySectors, distantSectors);
+			}
+
+			if (distantSectors != null)
+			{
+				// Remove all nearbySectors from distantSectors (deduplication)
+				distantSectors.ExceptWith(nearbySectors);
+			}
+		}
+
+		public static void FindObjectsInSectors(HashSet<Vector2i> sectors, List<ZDO> objects)
+		{
+			foreach (Vector2i sector in sectors)
+			{
+				ZDOMan.instance.FindObjects(sector, objects);
+			}
+		}
+
+		public static void FindDistantObjectsInSectors(HashSet<Vector2i> sectors, List<ZDO> objects)
+		{
+			foreach (Vector2i sector in sectors)
+			{
+				ZDOMan.instance.FindDistantObjects(sector, objects);
+			}
 		}
 	}
 }
